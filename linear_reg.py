ef4f69778848244cab4a361e11e7c14c1c594bbf
Omar Hosny <68732608+Omar-Hosni@users.noreply.github.com>
2023-04-08 01:40:08 +0200

    Create linear_reg.py

---
+++ linear_reg.py
@@ -0,0 +1,31 @@
+from sklearn import datasets
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+california = datasets.fetch_california_housing()
+
+#features / labels
+X = california.data
+y = california.target
+
+print(X.shape)
+print(y.shape)
+
+l_reg = linear_model.LinearRegression()
+
+plt.scatter(X.T[4], y)
+plt.show()
+
+
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
+
+l_reg.fit(X_train, y_train)
+
+predictions = l_reg.predict(X_test)
+#acc = accuracy_score(y_test, predictions)
+print('r^2 value: ', l_reg.score(X,y))
+print('coedd: ', l_reg.coef_) #slope
+
